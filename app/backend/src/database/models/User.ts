c7a43282c3d8c027110f33b6b443c0db49997e1f
Bianca Oura <biancaoura@gmail.com>
2023-01-20 21:51:37 -0300

    feat: User model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from 'sequelize';
+
+import sequelize from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.STRING,
+  },
+  username: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  sequelize,
+  tableName: 'users',
+  timestamps: false,
+});
