76a4fcc6491ae090fb58688f2762529bcdca0e77
Bianca Oura <biancaoura@gmail.com>
2023-01-23 17:56:15 -0300

    refactor(login): error handler on login validation fn

--- app/backend/src/middlewares/ValidateLogin.ts
+++ app/backend/src/middlewares/ValidateLogin.ts
@@ -1,13 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import HttpError from '../utils/HttpError';
 import ILogin from '../interfaces/ILogin';
 
 export default class ValidateLogin {
-  public loginFields = async (req: Request, res: Response, next: NextFunction) => {
+  public loginFields = async (req: Request, _res: Response, next: NextFunction) => {
     const { email, password }: ILogin = req.body;
 
     if (!email || !password) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
+      throw new HttpError(StatusCodes.BAD_REQUEST, 'All fields must be filled');
     }
     next();
   };
