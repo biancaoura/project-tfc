1e45f4740f5c4851d6872d87508370d3f7e4e6ba
Bianca Oura <biancaoura@gmail.com>
2023-01-26 18:30:03 -0300

    feat: leaderboard and points interfaces

--- app/backend/src/interfaces/IMatch.ts
+++ app/backend/src/interfaces/IMatch.ts
@@ -1,7 +1,7 @@
 import IGoals from './IGoals';
 
 export default interface IMatch extends IGoals {
-  id?: number;
+  id: number;
   homeTeamId: number;
   awayTeamId: number;
   inProgress?: boolean;
---
+++ app/backend/src/interfaces/IPoints.ts
@@ -0,0 +1,5 @@
+export default interface IPoints {
+  homeTeam: number;
+  awayTeam: number;
+  win: string;
+}
