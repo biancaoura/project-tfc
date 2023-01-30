6865c33530579d7e6ad6f5cf2c4eaa3d566b93e5
Bianca Oura <biancaoura@gmail.com>
2023-01-29 22:54:44 -0300

    test(matches): add POST and PATCH tests

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -8,7 +8,8 @@ import { app } from '../app';
 import Match from '../database/models/Match';
 
 import { Response } from 'superagent';
-import { matches } from './mocks/matchMock';
+import { matches, invalidMatch, matchWithInvalidTeam, validMatch, createdMatch, matchToEdit, editedMatch } from './mocks/matchMock';
+import { token } from './mocks/userMock';
 import { StatusCodes } from 'http-status-codes';
 
 chai.use(chaiHttp);
@@ -60,6 +61,76 @@ describe('Testing Matches route', function () {
 
       expect(response.status).to.be.equal(StatusCodes.OK);
       expect(response.body).to.deep.equal([matches[1]]);
+    });
+  });
+
+  describe('Testing POST /matches route', function () {
+
+    it('1 - Should create a new match if the teams exist', async function () {
+      sinon
+      .stub(Match, 'create')
+      .resolves(createdMatch as Match);
+
+      response = await chai
+      .request(app)
+      .post('/matches')
+      .set({ 'Authorization': token })
+      .send(validMatch);
+
+      expect(response.status).to.be.equal(StatusCodes.CREATED);
+    });
+
+    it('2 - Should throw an error when trying to create a new match with only one team', async function() {
+      response = await chai
+      .request(app)
+      .post('/matches')
+      .set({ 'Authorization': token })
+      .send(invalidMatch);
+      
+      expect(response.status).to.be.equal(StatusCodes.UNPROCESSABLE_ENTITY);
+      expect(response.body).to.deep.equal({ message: 'It is not possible to create a match with two equal teams' });
+    });
+    
+    it('3 - Should throw an error when trying to create a new match with an invalid team', async function () {
+      response = await chai
+      .request(app)
+      .post('/matches')
+      .set({ 'Authorization': token })
+      .send(matchWithInvalidTeam);
+      
+      expect(response.status).to.be.equal(StatusCodes.NOT_FOUND);
+      expect(response.body).to.deep.equal({ message: 'There is no team with such id!' });
+    });
+  });
+
+  describe('Testing PATCH /matches route', function () {
+    it('1 - Should update the correct match at /matches/1', async function () {
+      sinon
+      .stub(Match, 'findOne')
+      .resolves(editedMatch as any);
+
+      response = await chai
+      .request(app)
+      .patch('/matches/1')
+      .set({ 'Authorization': token })
+      .send(matchToEdit);
+
+      expect(response.status).to.be.equal(StatusCodes.OK);
+      expect(response.body).to.deep.equal(editedMatch);
+    });
+
+    it('2 - Should be able to mark a match as finished at /matches/1/finish', async function () {
+      sinon
+      .stub(Match, 'findOne')
+      .resolves(editedMatch as any);
+
+      response = await chai
+      .request(app)
+      .patch('/matches/1/finish')
+      .set({ 'Authorization': token });
+
+      expect(response.status).to.be.equal(StatusCodes.OK);
+      expect(response.body).to.deep.equal({ message: 'Finished' });
     });
   });
 });
--- app/backend/src/tests/mocks/matchMock.ts
+++ app/backend/src/tests/mocks/matchMock.ts
@@ -28,3 +28,53 @@ export const matches = [
     }
   }
 ];
+
+export const invalidMatch = {
+  homeTeamId: 2,
+  homeTeamGoals: 4,
+  awayTeamId: 2,
+  awayTeamGoals: 5,
+};
+
+export const matchWithInvalidTeam = {
+  homeTeamId: 200,
+  homeTeamGoals: 4,
+  awayTeamId: 1,
+  awayTeamGoals: 2,
+};
+
+export const validMatch = {
+  homeTeamId: 1,
+  homeTeamGoals: 2,
+  awayTeamId: 3,
+  awayTeamGoals: 4,
+};
+
+export const createdMatch = {
+  id: 3,
+  homeTeamId: 1,
+  homeTeamGoals: 2,
+  awayTeamId: 3,
+  awayTeamGoals: 4,
+  inProgress: true,
+};
+
+export const matchToEdit = {
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+};
+
+export const editedMatch = {
+  id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 2,
+    awayTeamId: 8,
+    awayTeamGoals: 2,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'São Paulo'
+    },
+    awayTeam: {
+      teamName: 'Grêmio'
+    }
+};
